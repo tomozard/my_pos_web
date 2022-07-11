0605087b715cb95b88d3720ac0351898770d65fc
tomozard <tomozard@gmail.com>
2022-07-11 17:16:15 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "styles/global.css";
+import { Provider } from "react-redux";
+import store from "redux/store";
+import "./i18n";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Home />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
