f22a76318b56551a3f8479973fa423cd68412485
tomozard <tomozard@gmail.com>
2022-07-12 13:31:54 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,32 @@ import { Provider } from "react-redux";
 import store from "redux/store";
 import "./i18n";
 
-import Login from "pages/login";
+// import Login from "pages/login";
 import { Routes, Route } from "react-router-dom";
 import Home from "pages";
-import { useLiff } from "react-liff";
-import { useEffect, useState } from "react";
-import { Button } from "./components/button/index";
+// import { useLiff } from "react-liff";
+// import { useEffect, useState } from "react";
 
 function App(): JSX.Element {
-  const [displayName, setDisplayName] = useState("");
-  const { error, isLoggedIn, isReady, liff } = useLiff();
+  // const [displayName, setDisplayName] = useState("");
+  // const { error, isLoggedIn, isReady, liff } = useLiff();
 
-  useEffect(() => {
-    if (!isLoggedIn) return;
+  // useEffect(() => {
+  //   if (!isLoggedIn) return;
 
-    (async () => {
-      const profile = await liff.getProfile();
-      setDisplayName(profile.displayName);
-    })();
-  }, [liff, isLoggedIn]);
-
-  const showDisplayName = () => {
-    if (error) return <p>Something is wrong.</p>;
-    if (!isReady) return <p>Loading...</p>;
-
-    if (!isLoggedIn) {
-      return (
-        <Button className="App-button" onClick={() => liff.login()}>
-          Login
-        </Button>
-      );
-    }
-    return (
-      <>
-        <p>Welcome to the react-liff demo app, {displayName}!</p>
-        <button className="App-button" onClick={liff.logout}>
-          Logout
-        </button>
-      </>
-    );
-  };
+  //   (async () => {
+  //     const profile = await liff.getProfile();
+  //     setDisplayName(profile.displayName);
+  //   })();
+  // }, [liff, isLoggedIn]);
 
   return (
     <ChakraProvider theme={theme}>
       <Provider store={store}>
         <div className="App">
-          <header className="App-header">{showDisplayName()}</header>
+          {/* <header className="App-header">{showDisplayName()}</header> */}
           <Routes>
-            <Route path="/login" element={<Login />} />
+            {/* <Route path="/login" element={<Login />} /> */}
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,54 @@
-import React from "react";
-import { Flex, Center, Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Flex, Center, Box, Button } from "@chakra-ui/react";
+
+import { useLiff } from "react-liff";
 
 const Home: React.FC = () => {
-  return (
-    <Flex direction="column" minH="100vh">
-      <Flex color="white" minH="100vh">
-        <Box width={72} bg="green.500">
-          <Center>Box 1</Center>
-        </Box>
-        <Box flex="1" bg="blue.500">
-          <Center>Box 1</Center>
-        </Box>
-        <Box width={360} bg="tomato">
-          <Center>Box 3</Center>
-        </Box>
-      </Flex>
-    </Flex>
-  );
+  const [displayName, setDisplayName] = useState("");
+  const { error, isLoggedIn, isReady, liff } = useLiff();
+  const showDisplayName = () => {
+    if (error) return <p>Something is wrong.</p>;
+    if (!isReady) return <p>Loading...</p>;
+
+    if (!isLoggedIn) {
+      return (
+        <Button className="App-button" onClick={() => liff.login()}>
+          Login
+        </Button>
+      );
+    }
+    return (
+      <>
+        <Flex direction="column" minH="100vh">
+          <Flex color="white" minH="100vh">
+            <Box width={72} bg="green.500">
+              <p>Welcome to the react-liff demo app, {displayName}!</p>
+              <button className="App-button" onClick={liff.logout}>
+                Logout
+              </button>
+            </Box>
+            <Box flex="1" bg="blue.500">
+              <Center>Box 1</Center>
+            </Box>
+            <Box width={360} bg="tomato">
+              <Center>Box 3</Center>
+            </Box>
+          </Flex>
+        </Flex>
+      </>
+    );
+  };
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    (async () => {
+      const profile = await liff.getProfile();
+      setDisplayName(profile.displayName);
+    })();
+  }, [liff, isLoggedIn]);
+
+  return <>{showDisplayName()}</>;
 };
 
 export default Home;
