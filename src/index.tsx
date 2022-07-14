c34b36169c6b2b8dff4f2edbe7e2b4879585566b
tomozard <tomozard@gmail.com>
2022-07-14 11:39:22 +0700

    add pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -19,7 +20,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
